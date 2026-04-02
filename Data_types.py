aba5a7de63aead5b43f596f36c49f03a52230776
reshma-devraj <reshmadevraj2230@gmail.com>
2026-04-02 16:06:18 +0530

    data types

---
+++ Data_types.py
@@ -0,0 +1,20 @@
+# Numeric data types
+a = 10
+b = 3.5
+
+print("Addition:", a + b)
+print("Division:", a / b)
+
+# String data types
+name = "Paul"
+greeting = "Hello " + name
+print(greeting)
+
+# Mixing types (will cause error if not handled)
+age = 20
+print("Age: " + str(age))  # convert number to string
+
+# Type checking
+print(type(a))
+print(type(b))
+print(type(name))
